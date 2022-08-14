78d5dd68f9ddc42cc3d09c962006c6dab6fd1927
Saklain Mustak <saklainmustak@sakmac.local>
2022-08-15 03:23:40 +0600

    get api set to show data

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
+app.use(cors());
+app.use(express.json())
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+//od6UizUmFvBVZw4z
+//crudBdcom
+
+const uri = "mongodb+srv://crudBdcom:od6UizUmFvBVZw4z@cluster0.xopxfjp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      const database = client.db("product");
+      const collection = database.collection("products");
+      //get
+      app.get('/products', async(req,res) =>{
+        const query = req.query;
+        const cursor = collection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      })
+      
+    } finally { }
+  }
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('welcome in user crud bdcom server site')
 })
 
 app.listen(port, () => {
